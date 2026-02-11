b9e71fcf36eb582fb95bb7ca19ac421d8f7d8163
Alex Artamonov <aartamonov@bsd220.org>
2026-02-11 17:41:19 -0600

    Initial Commit

---
+++ Country Flag Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country Flag Game
+//
+//  Created by Alex Artamonov on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Alex Artamonov on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
