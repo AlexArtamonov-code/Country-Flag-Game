cab4d0837b996ca4b497797b1e9c4248a3c14bd1
Alex Artamonov <aartamonov@bsd220.org>
2026-02-11 20:12:56 -0600

    Added Country Flag Images and added Question and Answer structs

---
+++ Country Flag Game/Models/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Country Flag Game
+//
+//  Created by Alex Artamonov on 2/11/26.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswers: [Answer]
+}
