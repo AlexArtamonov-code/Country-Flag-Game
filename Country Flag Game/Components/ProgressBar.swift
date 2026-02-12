524cb40eace8afe3b7e2cca4a62165ddf2373834
Alex Artamonov <aartamonov@bsd220.org>
2026-02-11 20:28:23 -0600

    Created Progress Bar

---
+++ Country Flag Game/Components/ProgressBar.swift
@@ -0,0 +1,29 @@
+//
+//  ProgressBar.swift
+//  Country Flag Game
+//
+//  Created by Alex Artamonov on 2/11/26.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress: CGFloat
+    var body: some View {
+        ZStack(alignment: .leading){
+            Rectangle()
+                .frame(maxWidth: 350, maxHeight: 4)
+                .foregroundStyle(.gray)
+                .cornerRadius(10)
+            Rectangle()
+                .frame(maxWidth: progress, maxHeight: 4)
+                .foregroundStyle(.yellow)
+                .cornerRadius(10)
+                
+        }
+    }
+}
+
+#Preview {
+    ProgressBar(progress: 50)
+}
